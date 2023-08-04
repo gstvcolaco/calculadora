49d65b587d42cf55002bf2debc5d2cb3f18117c3
gstvcolaco <124811513+gstvcolaco@users.noreply.github.com>
2023-08-04 14:33:14 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,25 @@
+
+function somar() {
+    A = document.getElementById('num1').value
+    B = document.getElementById('num2').value
+
+    alert(parseInt(A) + parseInt(B))
+}
+function subtrair() {
+    A = document.getElementById('num1').value
+    B = document.getElementById('num2').value
+
+    alert(parseInt(A) - parseInt(B))
+}
+function multiplicar() {
+    A = document.getElementById('num1').value
+    B = document.getElementById('num2').value
+
+    alert(parseInt(A) * parseInt(B))
+}
+function dividir() {
+    A = document.getElementById('num1').value
+    B = document.getElementById('num2').value
+
+    alert(parseInt(A) / parseInt(B))
+}
